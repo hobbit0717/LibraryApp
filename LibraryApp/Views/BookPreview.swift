33f1976adcb5cb2573f351461f7fa9f2f82c6480
Jeehun Choi <hobbit0717@gmail.com>
2021-08-07 15:37:43 -0400

    - Complete BookPreview, this View will be used to display an image of each book

--- LibraryApp/Views/BookPreview.swift
+++ LibraryApp/Views/BookPreview.swift
@@ -8,13 +8,44 @@
 import SwiftUI
 
 struct BookPreview: View {
+    
+    var book: Book
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Rectangle()
+                .foregroundColor(.white)
+                .shadow(color: .gray, radius: 5, x: -5, y: 5)
+            
+            VStack(alignment: .leading) {
+                HStack {
+                    Text(book.title)
+                        .font(.title)
+                        .bold()
+                    
+                    Spacer()
+                    
+                    if book.isFavourite {
+                        Image(systemName: "star.fill")
+                            .resizable()
+                            .frame(width: 28, height: 28)
+                            .foregroundColor(.yellow)
+                    }
+                }
+                Text(book.author)
+                    .italic()
+                
+                Image("cover\(book.id)")
+                    .resizable()
+                    .scaledToFit()
+            }
+            .padding()
+        }
     }
 }
 
 struct BookPreview_Previews: PreviewProvider {
     static var previews: some View {
-        BookPreview()
+        BookPreview(book: Book())
     }
 }
