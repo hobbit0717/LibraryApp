564fe6171eab8e0dd3f98e3b61203fdd39474d34
Jeehun Choi <hobbit0717@gmail.com>
2021-08-07 20:53:57 -0400

    - Complete BookDetailView, this view is displayed when user selects a book from BookListView.

--- LibraryApp/Views/BookDetailView.swift
+++ LibraryApp/Views/BookDetailView.swift
@@ -8,13 +8,60 @@
 import SwiftUI
 
 struct BookDetailView: View {
+    
+    @EnvironmentObject var model: BookModel
+    @State private var rating = 2
+    
+    var book: Book
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            NavigationLink(destination: BookContentView(book: book)) {
+                VStack {
+                    Text("Read Now!")
+                        .font(.title)
+                        .accentColor(.black)
+                    
+                    Image("cover\(book.id)")
+                        .resizable()
+                        .scaledToFit()
+                }
+            }
+            .padding()
+            
+            Text("Mark for later!")
+                .font(.headline)
+            
+            Button(action: { model.updateFavourite(forId: book.id) }) {
+                Image(systemName: book.isFavourite ? "star.fill" : "star")
+                    .resizable()
+                    .frame(width: 28, height: 28)
+                    .foregroundColor(.yellow)
+            }
+            
+            Text("Rate \(book.title)")
+                .font(.headline)
+            
+            Picker("Rate this book!", selection: $rating) {
+                ForEach(1..<6) { index in
+                    Text("\(index)")
+                        .tag(index)
+                }
+            }
+            .pickerStyle(SegmentedPickerStyle())
+            .padding([.leading, .trailing, .bottom], 60)
+            .onChange(of: rating, perform: { value in
+                model.updateRating(forId: book.id, rating: rating)
+            })
+        }
+        .onAppear { rating = book.rating }
+        .navigationBarTitle("\(book.title)")
     }
 }
 
 struct BookDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        BookDetailView()
+        BookDetailView(book:Book())
+            .environmentObject(BookModel())
     }
 }
